6768de512a1558b8e96c37244b685e67863bfd3e
ShehanMaxwell <shehanbbn@gmail.com>
2018-11-09 20:07:33 +0530

    uploadin 3

---
+++ precache-manifest.81f5ec0f68e07e6a7eb1b33efdafeaec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "291baa54e19ce4132fcc",
+    "url": "/static/js/main.291baa54.chunk.js"
+  },
+  {
+    "revision": "91b210bb715a68ac9738",
+    "url": "/static/js/1.91b210bb.chunk.js"
+  },
+  {
+    "revision": "291baa54e19ce4132fcc",
+    "url": "/static/css/main.3100a549.chunk.css"
+  },
+  {
+    "revision": "91b210bb715a68ac9738",
+    "url": "/static/css/1.2fcff2a0.chunk.css"
+  },
+  {
+    "revision": "ebedd61a73f798d305b4fb867d6a7cf0",
+    "url": "/index.html"
+  }
+];
